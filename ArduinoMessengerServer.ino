1c0319ecc31eec6b1ef2113e2c966c5bf0f89bde
KamilLapotop <kamil.tokarzewski@gmail.com>
2017-01-19 13:34:36 +0100

    Updated README.md (Prerequisites, Wiring diagram) Updated ArduinoMessengerServer.ino (ArduinoMessenger 0.2.0 support)

--- ArduinoMessengerServer.ino
+++ ArduinoMessengerServer.ino
@@ -1,20 +1,18 @@
+#include <Arduino.h>
 #include <ArduinoMessenger.h>
 #include <UIPEthernet.h>
 #include <IRremote.h>
+
 #define GET_PIN 6
 #define SEND_PIN 4
 #define ERROR_PIN 5
 #define IRRECV_PIN 7
 #define FACTOR 50
 
-//==================== Memory Control ==================//
-const PROGMEM char prefix[] = "SEND\n{\"resource\":\"";
-const PROGMEM char separator[] = "\",\"value\":\"";
-const PROGMEM char suffix[] = "\",\"id\":\"prototype\"};";
-char mem_buf[25];
-
 EthernetServer server = EthernetServer(5544);
-Messenger messenger;
+EthernetClient client;
+
+Messenger messenger(client);
 message m;
 char function[5];
 
@@ -22,8 +20,10 @@ IRrecv irrecv(IRRECV_PIN);
 IRsend irsend; //pin 13
 decode_results results;
 
+boolean codeReady = false;
 byte codeLen;
 unsigned int rawCodes[RAWBUF];
+
 void storeCode(decode_results *results) {
   codeLen = results->rawlen - 1;
   for (int i = 1; i <= codeLen; i++) {
@@ -35,6 +35,7 @@ void storeCode(decode_results *results) {
     }
   }
 }
+
 void sendCodeToIR(int frequency) {
   irsend.sendRaw(rawCodes, codeLen, frequency);
 }
@@ -47,11 +48,11 @@ bool compareType(const char* type) {
     return false;
   }
 }
+
 void copyCodeRequest() {
   byte size = strlen(m.request);
   strncpy(function, m.request, size);
 }
-
 
 void decode(char * msg) {
   codeLen = 0;
@@ -92,39 +93,28 @@ bool extractSubsequence(char * sub) {
   }
 }
 
-
-void printRawCode(EthernetClient client) {
+void printRawCode() {
   for (byte i = 0; i < codeLen - 1; i++) {
-    client.print(rawCodes[i] / FACTOR);
-    client.print(F(" "));
+    messenger.print(rawCodes[i] / FACTOR);
+    messenger.print(" ");
   }
-  client.print(rawCodes[codeLen - 1] / FACTOR);
+  messenger.print(rawCodes[codeLen - 1] / FACTOR);
 }
 
-void sendMessage(char * buttonId, EthernetClient client) {
-  // copy preffix, separator and suffix to buffer using strcpy_P() before print
-  strcpy_P(mem_buf, prefix);
-  client.print(mem_buf); // sending prefix
-
-  client.print(buttonId);
-
-  strcpy_P(mem_buf, separator);
-  client.print(mem_buf); // sending separator
-
-  printRawCode(client);
-  //encode();
-
-  strcpy_P(mem_buf, suffix);
-  client.print(mem_buf); // sending suffix
-
-  client.flush();
+void sendMessage(char * buttonId) {
+  messenger.printPROGMEM(PUT);
+  messenger.print('\n');
+  messenger.printPROGMEM(request_prefix);
+  messenger.print(buttonId);
+  messenger.printPROGMEM(value_prefix);
+  printRawCode();
+  messenger.printPROGMEM(suffix);
 }
 
 void ledsOff(){
   digitalWrite(SEND_PIN, LOW);
   digitalWrite(GET_PIN, LOW);
   digitalWrite(ERROR_PIN, LOW);
-
 }
 
 
@@ -138,43 +128,27 @@ void setup()
   pinMode(SEND_PIN, OUTPUT);
   pinMode(GET_PIN, OUTPUT);
   pinMode(ERROR_PIN, OUTPUT);
-
 }
-boolean codeReady = false;
-EthernetClient client;
+
 
 void loop() {
-  size_t size;
 
   if (EthernetClient current = server.available()) {
     client = current;
-
-    if (client) {
-      while ((size = client.available()) > 0) {
+    if (client.connected()) {
+      if(messenger.parseMessage(&m)){
         ledsOff();
-        if (size < M_BUF) {
-          uint8_t* msg = (uint8_t*)malloc(size);
-          size = client.read(msg, size);
-          if(!messenger.parseMessage(&m, msg, size)){
-            digitalWrite(ERROR_PIN,HIGH);
-          }
-          
-          free(msg);
-        }
-        else {
-          digitalWrite(ERROR_PIN,HIGH);
-          client.flush();
-        }
+      } else{
+        digitalWrite(ERROR_PIN, HIGH);
       }
     }
-
   }
-  
+
   if (compareType(GET)) {
     digitalWrite(GET_PIN, HIGH);
     copyCodeRequest();
     irrecv.enableIRIn();
-  } else if (compareType(SEND)) {
+  } else if (compareType(PUT)) {
     decode(m.resource);
     sendCodeToIR(atoi(m.value));
   }
@@ -187,12 +161,11 @@ void loop() {
   }
   if (codeReady) {
     if (client.connected()) {
-      sendMessage(function, client);
+      sendMessage(function);
       codeReady = false;
       digitalWrite(GET_PIN, LOW);
     } else {
       digitalWrite(ERROR_PIN, HIGH);
-
     }
   }
 
--- README.md
+++ README.md
@@ -1,3 +1,54 @@
 # ArduinoMessengerServer
 
-Arduino Leonardo with ENC28j60 as a TCP/IP server with IR receiver and transmiter.
+Sketch prepared for **Arduino Leonardo** with **ENC28J60** as a TCP/IP server with IR receiver and transmitter.
+
+##Prerequisites
+
+Download and extract following libraries in yours IDE/libraries directory:
+- [IRremote]  for Infrared communication,
+- [UIPEthernet] for handling TCP/IP connection by ENC28J60,
+- [ArduinoMessenger] to parse incoming messages.
+
+**Important**:
+To fit this skecht in Arduino Leonardo flash memory edit `IRremote.h` from IRremote library.
+In `Supported IR protocols`section disable `DECODE_` and `SEND_` includes for all protocols.
+
+
+### Wiring diagram
+![Sketch wiring diagram](/wiring/remote.png)
+
+This skecht uses Tsop2236 as IR receiver.
+
+![Tsop2236 pinout](/wiring/tsop2236.png)
+
+
+### Issues management
+Issues are managed at the GitHub [project issues page][issues].
+
+Everybody is allowed to report bugs or ask for features.
+
+### Getting the code
+The latest version of the code can be found at the [GitHub project page][scm].
+
+Feel free to fork it, and share the changes.
+## Collaborate
+
+The project is still under ongoing development, and any help will be well received.
+
+There are two ways to help: reporting errors and asking for extensions through the issues management, or forking the repository and extending the project.
+
+## Authors
+
+* **Kamil Tokarzewski** - *Initial work* - [ktokarzewski](https://github.com/ktokarzewski)
+
+See also the list of [contributors](https://github.com/ktokarzewski/ArduinoMessengerServer/contributors) who participated in this project.
+## License
+The project is released under the [MIT License][license].
+
+[issues]:https://github.com/ktokarzewski/ArduinoMessengerServer/issues
+[scm]:https://github.com/ktokarzewski/ArduinoMessengerServer
+[license]: http://www.opensource.org/licenses/mit-license.php
+
+[IRremote]:https://github.com/z3t0/Arduino-IRremote
+[UIPEthernet]:https://github.com/ntruchsess/arduino_uip
+[ArduinoMessenger]:https://github.com/ktokarzewski/ArduinoMessenger
